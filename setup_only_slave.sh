2443a661b853c5f890c6e78a01195b39bb17a1ae
bswsahoo11111 <32266851+bswsahoo11111@users.noreply.github.com>
2020-12-07 10:29:27 +0530

    Update setup_only_slave.sh

--- setup_only_slave.sh
+++ setup_only_slave.sh
@@ -7,5 +7,5 @@ for i in `cat hosts`
 do 
 ssh -t $i yum -y install  krb5-libs krb5-workstation
 scp -r $LOC/krb5.conf.default root@$i:/tmp/
-ssh -t $i cat /tmp/krb5.conf.default > /etc/krb5.conf
+ssh -t $i mv /tmp/krb5.conf.default  /etc/krb5.conf
 done
